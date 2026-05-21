2a1840fb17b13541cb8a20e32668f89f8549a598
Praveen Roy <roykumar0593@gmail.com>
2026-05-21 22:36:57 +0530

    added github

--- Typescript/github.ts
+++ Typescript/github.ts
@@ -4,7 +4,7 @@
 // We can download git from https://git-scm.com/install/windows and install in our local
 
 
-// 1. You are the first person to start test automation - NO REPO AVAILABLE
+// 1. You are the first employee to start test automation - NO REPO AVAILABLE
 
     // 0. Create a new repository on the version control tool
     // 1. Create a project and write the test cases inside your local system
@@ -12,10 +12,42 @@
     // 3. "git add ." - Add all the files that you have changes/added in your 
     // local git area
     // 4. "git commit -m "first commit" " - Commit all the changes to the local git with a message
-    // 5. "git branc -M master" - Create a new branch named as "master"
+    // 5. "git branch -M master" - Create a new branch named as "master"
     // 6. "git branch" - List all the branches that in your repository
     // 7. "git remote add origin https://github.com/Praveenroy05/TestPW.git" - Adds 
     // a remote repo to your local repo and link it with the remote repo
     // 8. "git push -u origin master" - Push the committed changes from the local
     // "master" branch to remote repository
 
+    // 9. git checkout master - Move to the master brach and pull the latest changes
+
+    // 10. On the master branch us - git pull
+
+    // 11. git checkout -b branchName - Create a new branch going forward for all the new task - 
+    // 12. Complete writing the test case and follow the steps from #3, #4 and #8(change the branch name)
+
+    // follow # 9 - #12
+
+
+// 2. If the repository is already available
+
+    // 0. git clone repoURL - Clone the respository in your local System
+    // 1. npm install - Install all the dependencies
+    // 2. git checkout -b featureBranch - Create a new branch for all the task going forward
+    // 3. git branch - to list out all the branches that are available in your local
+    // 4. Write the test case in your local system and save all the file
+    // 5. git status - Validate all the files that you have changed/added/deleted
+    // 6. git add . - Add all the changed files to your local git area
+    // 7. git commit -m "message" - Commit all the changes to your local git with message
+       // 7.1. git checkout master - Switch to the master branch
+       // 7.2. git pull - Pull all the latest changes grom your remote master branch to the local master branch
+       // 7.3. git checkout featureBranch - Switch back to the feature branch
+       // 7.4. git merge master / git rebase master - Merge master branch latest changes to featureBranch
+
+    // 8. git push -u origin featureBranch - Push the commited changes from the feature branc to the remote featurebranch
+    // 9. Goto Github/gitlab/bitbucket and create a pull request (PR) to merge your changes to the master branch
+
+
+    // Follow #2 - #9
+
+    // git stash - Stash the changes that you have done in your local and want to switch to other branch without commiting the changes
